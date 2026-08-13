a52756df4e56562928230198f9fabbf013cc1344
RYNO-JASH <goklanijash@gmail.com>
2026-08-13 16:08:01 +0530

    Update prac-2_2.cpp

--- PRACTICAL-2/prac-2_2.cpp
+++ PRACTICAL-2/prac-2_2.cpp
@@ -1,79 +1,110 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-int binaryFindBookCode(vector<int> &arr,int n,int key)
+
+int binaryFindBookCode(vector<int> &arr, int n, int key)
 {
-    int low = 0 , high = n-1;
-    while(low<=high)
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
     {
-        int mid = (low+high)/2;
-        
-        if(arr[mid]==key)
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key)
             return mid;
-        else if(arr[mid]>key)
-            high = mid-1;
+
+        else if (arr[mid] > key)
+            high = mid - 1;
+
         else
-            low = mid+1;
+            low = mid + 1;
     }
+
     return -1;
 }
-int recursivebinaryFindBookCode(vector<int> &arr,int low,int high,int key)
-{
-    if(low>high)
-        return -1;
-    while(low<=high)
-    {
-        int mid = low + (high-low)/2;
-        
-        if(arr[mid]==key)
-            return mid;
-        else if(arr[mid]<=key)
-            return recursivebinaryFindBookCode(arr,mid+1,high,key);
-        else
-            return recursivebinaryFindBookCode(arr,low,mid-1,key);
-    }
-}
 
+int recursivebinaryFindBookCode(vector<int> &arr, int low, int high, int key)
+{
+    // No elements left
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+
+    else if (arr[mid] > key)
+        return recursivebinaryFindBookCode(arr, low, mid - 1, key);
+
+    else
+        return recursivebinaryFindBookCode(arr, mid + 1, high, key);
+}
 
 int main()
 {
-    int n,key=0,type;
-    cout<<"Enter num of Book Size : ";
-    cin>>n;
+    int n, key, type;
+
+    cout << "Enter num of Book Size : ";
+    cin >> n;
+
     vector<int> arr(n);
 
-    for(int i = 0 ; i<n ; i++)
+    cout << "Enter Book Codes : " << endl;
+
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cout<<"Enter type for normal linear or recursive linear (1 or 2)";
-    cin>>type;
-    if(type==1 || type ==2)
+
+    // Binary Search requires sorted array
+    sort(arr.begin(), arr.end());
+
+    cout << "Sorted Book Codes : ";
+
+    for (int i = 0; i < n; i++)
     {
-        if(type==1)
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    cout << "Enter type for normal binary or recursive binary (1 or 2): ";
+    cin >> type;
+
+    if (type == 1 || type == 2)
+    {
+        cout << "Enter the number to search: ";
+        cin >> key;
+
+        int value;
+
+        if (type == 1)
         {
-            cout<<"Enter the num ! to search ? ";
-            cin>>key;
-            int value = binaryFindBookCode(arr,n,key);
-            if(value>=0)
-                cout<<"Found Book Code = "<<arr[value]<<endl<<"At : "<<value+1<<" position"<<endl;
-            else
-                cout<<"Book Code not found !!! "<<endl;
+            value = binaryFindBookCode(arr, n, key);
         }
-        if(type==2)
+        else
         {
-            cout<<"Enter the num ! to search ? ";
-            cin>>key;
-            int value = recursivebinaryFindBookCode(arr,0,n-1,key);
-            if(value>=0)
-                cout<<"Found Book Code = "<<arr[value]<<endl<<"At : "<<value+1<<" position"<<endl;
-            else
-                cout<<"Book Code not found !!! "<<endl;
+            value = recursivebinaryFindBookCode(arr, 0, n - 1, key);
+        }
+
+        if (value >= 0)
+        {
+            cout << "Found Book Code = " << arr[value] << endl;
+            cout << "At position = " << value + 1 << endl;
+        }
+        else
+        {
+            cout << "Book Code not found !!!" << endl;
         }
     }
     else
-        cout<<"Invalid input "<<endl;
-        
-    return 0 ;
+    {
+        cout << "Invalid input" << endl;
+    }
+
+    return 0;
 }
